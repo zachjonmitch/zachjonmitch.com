d8be9759d952017890b9cd42112cbac8425d1961
zjmitche <zjmitche@gmail.com>
2018-12-14 14:01:50 -0700

    Portfolio update

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     var $portfolio = $('#portfolio');
     var $whole = $('html, body');
-    var $a = $('a');
+    var $anchor = $('.fa-angle-down');
     
     var toggleScroll = false;
     
     //Smooth scroll anchor
-    $a.click(function(){
+    $anchor.click(function(){
         $whole.animate({
             scrollTop: $portfolio.offset().top
         }, 'slow');
